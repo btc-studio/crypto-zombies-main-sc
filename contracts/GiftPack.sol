94ed61cd100f88400a2014943e34fa5ee6cb9630
btcs-longnp <94104849+btcs-longnp@users.noreply.github.com>
2022-11-02 13:46:51 +0700

    [CZB-505] change logic openStarterPack (#33)
    * CZB-505 change logic openStarterPack
    
    * fix comment
    
    Co-authored-by: Long <longnp@btc-studio.io>

--- contracts/GiftPack.sol
+++ contracts/GiftPack.sol
@@ -5,13 +5,19 @@ import "./ZombieAttack.sol";
 
 contract GiftPack is ZombieAttack {
     uint public constant STATER_ZOMBIE_COUNT = 3;
+    mapping(address => bool) seenWalletOpenStaterPack;
 
     event OpenStaterPack(address indexed owner, Zombie[] zombies);
 
     constructor(address _token) ZombieAttack(_token) {}
 
+    function checkOpenStarterPack() public view returns (bool) {
+        return seenWalletOpenStaterPack[msg.sender];
+    }
+
     function openStaterPack() public {
-        require(_getNumberZombiesOfOwner(msg.sender) < STATER_ZOMBIE_COUNT);
+        require(!checkOpenStarterPack());
+        seenWalletOpenStaterPack[msg.sender] = true;
 
         emit OpenStaterPack(msg.sender, createManyZombie(STATER_ZOMBIE_COUNT));
     }
---
+++ test/Giftpack.test.js
@@ -0,0 +1,41 @@
+const { expect, assert } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('GiftPack', () => {
+  let mainSmartContract, btcsContract;
+  let deployer, addr1, addr2;
+
+  beforeEach(async () => {
+    const btcsTokenContractFactory = await ethers.getContractFactory("BTCSToken");
+    const mainSmartContractFactory = await ethers.getContractFactory("GiftPack");
+    [deployer, addr1, addr2] = await ethers.getSigners();
+
+    btcsContract = await btcsTokenContractFactory.deploy();
+    mainSmartContract = await mainSmartContractFactory.deploy(btcsContract.address);
+  });
+
+  describe('openStaterPack', async () => {
+    it('Should return false if new wallet checkOpenStarterPack', async () => {
+      const isOpenStaterPack = await mainSmartContract.connect(addr1).checkOpenStarterPack();
+      expect(isOpenStaterPack).to.be.false;
+    });
+
+    it('Should openStaterPack without error', async () => {
+      expect(await mainSmartContract.connect(addr1).openStaterPack()).to.not.be.undefined;
+    });
+
+    it('Should return true if wallet already opened stater pack', async () => {
+      await mainSmartContract.connect(addr1).openStaterPack();
+      expect(await mainSmartContract.connect(addr1).checkOpenStarterPack()).to.be.true;
+    });
+
+    it('Should throw error openStaterPack if wallet already opened stater pack', async () => {
+      await mainSmartContract.connect(addr1).openStaterPack();
+      try {
+        await mainSmartContract.connect(addr1).openStaterPack();
+      } catch (err) {
+        expect(err).to.not.be.undefined;
+      }
+    });
+  });
+});
