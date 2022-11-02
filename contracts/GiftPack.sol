347cefc1bc83fddefb6b44c562a954937f37efa8
Long <longnp@btc-studio.io>
2022-11-02 11:15:11 +0700

    CZB-505 change logic openStarterPack

--- contracts/GiftPack.sol
+++ contracts/GiftPack.sol
@@ -5,13 +5,19 @@ import "./ZombieAttack.sol";
 
 contract GiftPack is ZombieAttack {
     uint public constant STATER_ZOMBIE_COUNT = 3;
+    mapping(address => bool) seenOpenStaterPack;
 
     event OpenStaterPack(address indexed owner, Zombie[] zombies);
 
     constructor(address _token) ZombieAttack(_token) {}
 
+    function checkOpenStaterPack() public view returns (bool) {
+        return seenOpenStaterPack[msg.sender];
+    }
+
     function openStaterPack() public {
-        require(_getNumberZombiesOfOwner(msg.sender) < STATER_ZOMBIE_COUNT);
+        require(!checkOpenStaterPack());
+        seenOpenStaterPack[msg.sender] = true;
 
         emit OpenStaterPack(msg.sender, createManyZombie(STATER_ZOMBIE_COUNT));
     }
